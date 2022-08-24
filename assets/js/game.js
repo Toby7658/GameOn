13d1072146772a23a8afdb5acf7af650bf04ae98
Toby7658 <orlaghtobin@gmail.com>
2022-08-24 14:15:12 +0000

    Add resize character images

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,8 +1,12 @@
+
+
+
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
 canvas.width = 550;
 canvas.height = 600;
+
 
 function gameLoop() {
     ctx.fillStyle = "black";
