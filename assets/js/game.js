1d4436a62d0e09f457149a4bf161cc706490cbcb
Toby7658 <orlaghtobin@gmail.com>
2022-08-24 13:11:22 +0000

    Add gradient background behind canvas

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,7 +1,12 @@
 const canvas = document.getElementById("game");
-const ctx = canvas.getContext("2D")
+const ctx = canvas.getContext("2d");
 
 canvas.width = 550;
 canvas.height = 600;
 
-function gameLoop() {}
+function gameLoop() {
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+setInterval(gameLoop, 1000 / 60);
