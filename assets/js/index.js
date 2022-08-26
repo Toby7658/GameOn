fac841a9e2ac9f644091650412571c8fef4b6166
Toby7658 <orlaghtobin@gmail.com>
2022-08-26 11:10:04 +0000

    Add increase to ship size

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,10 +5,10 @@ const canvas = document.getElementById('canvas');
 
 
 // setting width and height to cover full screen
-//canvas.width = window.innerHeight;
-//canvas.height = window.innerWidth;
-//canvasContext.fillStyle = "black";
-//canvasContext.fillRect(0, 0, canvas.width, canvas.height); 
+// canvas.width = window.innerHeight;
+// canvas.height = window.innerWidth;
+// canvasContext.fillStyle = "black";
+// canvasContext.fillRect(0, 0, canvas.width, canvas.height); 
 
 // code to create keyboard controls
 // window.addEventListener("keydown", (e) => {
@@ -50,51 +50,6 @@ window.addEventListener('keydown', (e) => {
           control.style.top = parseInt(window.getComputedStyle(control).getPropertyValue("top")) + moveBy + 'px';
             break;
 
-            
-  if (e.key == "ArrowUp" || e.keyCode == 32) {
-    //32 is for space key
-    var bullets = document.createElement("div");
-    bullet.classList.add("laser");
-    game.appendChild(bullet);
-
-    var movebullet = setInterval(() => {    //repeated
-      var rocks = document.getElementsByClassName("aliengirl");
-
-      for (var i = 0; i < rocks.length; i++) {
-        var rock = rocks[i];
-        if (rock != undefined) {
-          var rockbound = rock.getBoundingClientRect();
-          var bulletbound = bullet.getBoundingClientRect();
-
-          //Condition to check whether the rock/alien and the bullet are at the same position..!
-          //If so,then we have to destroy that rock
-
-          if (
-            bulletbound.left >= rockbound.left &&
-            bulletbound.right <= rockbound.right &&
-            bulletbound.top <= rockbound.top &&
-            bulletbound.bottom <= rockbound.bottom
-          ) {
-            rock.parentElement.removeChild(rock); //Just removing that particular rock;
-            //Scoreboard
-            document.getElementById("score").innerHTML =
-              parseInt(document.getElementById("score").innerHTML) + 1;
-          }
-        }
-      }
-      var bulletbottom = parseInt(
-        window.getComputedStyle(bullet).getPropertyValue("bottom")
-      );
-
-      //Stops the bullet from moving outside the gamebox
-      if (bulletbottom >= 800) {
-        clearInterval(movebullet);
-      }
-
-      bullet.style.left = left + "px"; //bullet should always be placed at the top of my ship..!
-      bullet.style.bottom = bulletbottom + 10 + "px";
-    });
-  }
 }
 
 var generaterocks = setInterval(() => {
@@ -104,11 +59,11 @@ var generaterocks = setInterval(() => {
   var rockleft = parseInt(
     window.getComputedStyle(rock).getPropertyValue("left")
   );
-  //generate value between 0 to 450 where 450 => gameboard width - rock width
-  rock.style.left = Math.floor(Math.random() * 3450) + "px";
+  //generate value => gameboard width - rock width
+  rock.style.left = Math.floor(Math.random() * 1700) + "px";   // aliens fall width of screen
 
   canvas.appendChild(rock);
-}, 1000);
+}, 3000);  // aliens numbers spaced out as they fall
 
 var moverocks = setInterval(() => {
   var rocks = document.getElementsByClassName("aliengirl");
@@ -118,19 +73,20 @@ var moverocks = setInterval(() => {
       //Now I have to increase the top of each rock,so that the rocks can move downwards..
       var rock = rocks[i]; //getting each rock
       var rocktop = parseInt(
-        window.getComputedStyle(rock).getPropertyValue("top")
-      );
+       window.getComputedStyle(rock).getPropertyValue("top")
+       );
       //475 => game board height - rockheight + 25
       if (rocktop >= 800) {
         alert("Game Over");
-        clearInterval(moverocks);
-        window.location.reload();
+        // document.location.reload();
+        // clearInterval(interval);
       }
+      
 
-      rock.style.top = rocktop + 25 + "px";
+      rock.style.top = rocktop + 1 + "px";
     }
   }
-}, 650);  // speed of alien 
+}, 1000);  // speed of alien 
     }
 );
 
