8f1960aa3090a1b119d7ebe077c0caa82e062c4a
Toby7658 <orlaghtobin@gmail.com>
2022-08-28 14:12:28 +0000

    Add new alien image for better visual

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,6 +3,7 @@ let multiplyAlienGirl
 let moveAlienGirl
 let moveLaser
 let ship = document.getElementById('rocketship');
+let start=1;
 //function startGame() {}
 
 function letsPlay() {
@@ -19,6 +20,10 @@ function multiplyAlien() {
   },2000)
 }
 
+function updateScore(){
+  document.getElementById("score").innerHTML = parseInt(document.getElementById("score").innerHTML) + 1; 
+}
+
 function kill(b){
   const alienGirls = document.getElementsByClassName("aliengirl");
   for (let i = 0; i < alienGirls.length; i++) {
@@ -28,12 +33,14 @@ function kill(b){
     parseInt(window.getComputedStyle(b).getPropertyValue("left")) <= parseInt(window.getComputedStyle(alienGirls[i]).getPropertyValue("left")) + 50 
     ) {
       
-      alienGirls[i].remove();  
+      alienGirls[i].remove(); 
+      b.remove();
+      updateScore();
     }   
   }
 }
 
-function moveBullet() {
+/*function moveBullet() {
   moveLaser = setInterval(() => {
    const bullets = document.getElementsByClassName("laser");
    for (let i = 0; i < bullets.length; i++) {
@@ -45,8 +52,20 @@ function moveBullet() {
     kill(bullets[i]);
   }
  }, 300);
-}
+}*/
 
+
+function moveBullet(bull){
+  moveLaser = setInterval(() => {
+  if (parseInt(window.getComputedStyle(bull).getPropertyValue("top")) >  30) {
+       bull.style.top = parseInt(window.getComputedStyle(bull).getPropertyValue("top")) - 30 + 'px';
+     } else {
+       bull.remove()
+     }
+     kill(bull);
+   
+  }, 100);
+}
 
 function shoot(){
   let bullet = document.createElement("div");
@@ -54,28 +73,30 @@ function shoot(){
   bullet.style.left = parseInt(window.getComputedStyle(ship).getPropertyValue("left")) +35 + "px"; 
   bullet.style.top = parseInt(window.getComputedStyle(ship).getPropertyValue("top")) - 10 + "px"; 
   canvas.appendChild(bullet);
-  moveBullet();
+  //moveBullet();
+  moveBullet(bullet);
 }
 
 function moveAlien() {
    moveAlienGirl = setInterval(() => {
     const alienGirls = document.getElementsByClassName("aliengirl");
-    //var alienGirlTop = window.getComputedStyle(alienGirl).getPropertyValue("top");
-    //var alienGirlTopAsInteger = parseInt(alienGirlTop);
     for (let i = 0; i < alienGirls.length; i++) {
      alienGirls[i].style.top = parseInt(window.getComputedStyle(alienGirls[i]).getPropertyValue("top")) + 30 + 'px';
    
      if (parseInt(window.getComputedStyle(alienGirls[i]).getPropertyValue("top")) >  window.innerHeight - 100) {
-       alert("Game Over");
-       clearInterval(moveAlienGirl);
-       clearInterval(multiplyAlienGirl);
-       document.location.reload();
+      const el = document.createElement('div');
+      el.setAttribute('id', 'splash');
+      
+      el.innerHTML = 'GAME OVER<br />Press Enter to Restart';
+      canvas.appendChild(el);
+      clearInterval(moveAlienGirl);
+      clearInterval(multiplyAlienGirl);
+      clearInterval(moveBullet);
+      start=0;
      }
    }
   }, 1000);
 }
-
-
 
  window.addEventListener('keydown', (e) => {
   let moveBy = 20;
@@ -93,114 +114,15 @@ function moveAlien() {
       ship.style.top = parseInt(window.getComputedStyle(ship).getPropertyValue("top")) + moveBy + 'px';
       break;
     case (e.key === 'Enter'):
-      document.getElementById("splash").remove();
-      letsPlay();
+      if(start===1){
+        document.getElementById("splash").remove();
+        letsPlay();
+      } else {
+        document.location.reload();
+      }
       break;
     case (e.key === ' '):
       shoot();
     break;
   }
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*if (rocktop >=  window.innerHeight - 100) {
-     alert("Game Over");
-     document.location.reload();
-     clearInterval(moverocks);
-     clearInterval(creatrocks);
-   } */
-
-
-
-//   if (e.key == "ArrowUp" || e.keyCode == 32) {
-//     //32 is for space key
-//     var bullets = document.createElement("div");
-//     bullet.classList.add("laser");
-//     game.appendChild(bullet);
-
-//     var movebullet = setInterval(() => {    //repeated
-//       var rocks = document.getElementsByClassName("aliengirl");
-
-//       for (var i = 0; i < rocks.length; i++) {
-//         var rock = rocks[i];
-//         if (rock != undefined) {
-//           var rockbound = rock.getBoundingClientRect();
-//           var bulletbound = bullet.getBoundingClientRect();
-
-//           //Condition to check whether the rock/alien and the bullet are at the same position..!
-//           //If so,then we have to destroy that rock
-
-//           if (
-//             bulletbound.left >= rockbound.left &&
-//             bulletbound.right <= rockbound.right &&
-//             bulletbound.top <= rockbound.top &&
-//             bulletbound.bottom <= rockbound.bottom
-//           ) {
-//             rock.parentElement.removeChild(rock); //Just removing that particular rock;
-//             //Scoreboard
-//             document.getElementById("score").innerHTML =
-//               parseInt(document.getElementById("score").innerHTML) + 1;
-//           }
-//         }
-//       }
-//       var bulletbottom = parseInt(
-//         window.getComputedStyle(bullet).getPropertyValue("bottom")
-//       );
-
-//       //Stops the bullet from moving outside the gamebox
-//       if (bulletbottom >= 800) {
-//         clearInterval(movebullet);
-//       }
-
-//       bullet.style.left = left + "px"; //bullet should always be placed at the top of my ship..!
-//       bullet.style.bottom = bulletbottom + 10 + "px";
-//     });
-//   }
-// });
-
-// var generaterocks = setInterval(() => {
-//   var rock = document.createElement("div");
-//   rock.classList.add("aliengirl");
-//   //Just getting the left of the rock to place it in random position...
-//   var rockleft = parseInt(
-//     window.getComputedStyle(rock).getPropertyValue("left")
-//   );
-//   //generate value between 0 to 450 where 450 => gameboard width - rock width
-//   rock.style.left = Math.floor(Math.random() * 3450) + "px";
-
-//   canvas.appendChild(rock);
-// }, 1000);
-
-// var moverocks = setInterval(() => {
-//   var rocks = document.getElementsByClassName("aliengirl");
-
-//   if (rocks != undefined) {
-//     for (var i = 0; i < rocks.length; i++) {
-//       //Now I have to increase the top of each rock,so that the rocks can move downwards..
-//       var rock = rocks[i]; //getting each rock
-//       var rocktop = parseInt(
-//         window.getComputedStyle(rock).getPropertyValue("top")
-//       );
-//       //475 => game board height - rockheight + 25
-//       if (rocktop >= 800) {
-//         alert("Game Over");
-//         clearInterval(moverocks);
-//         window.location.reload();
-//       }
-
-//       rock.style.top = rocktop + 25 + "px";
-//     }
-//   }
-// }, 450);  // speed of alien 
